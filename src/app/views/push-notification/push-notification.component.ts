0e62590d9e44a40a55bd29e0695f7e543126775b
Mujtaba Alboori <mujtaba.boori@gmail.com>
2018-11-04 10:33:51 +0300

    More update on notification

--- src/app/views/push-notification/push-notification.component.ts
+++ src/app/views/push-notification/push-notification.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-push-notification',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PushNotificationComponent implements OnInit {
 
-  constructor() { }
+    pushNotificationData = {
+        titleEn : '',
+        titleAr : '',
+        messageEn: '',
+        messageAr: '',
+        language: '',
+        phone_type: '',
+        app_version: '',
+        status: '',
+        hasSentGift: '',
+        created: {}
+
+    };
+
+    users = {
+        count: 0,
+        items: []
+    };
+
+
+    SendPushNotification() {
+
+        Object.assign(this.pushNotificationData, {sendNotification: true});
+        this.filterItem();
+        // alert(JSON.stringify(this.pushNotificationData));
+    }
+
+    filterItem(payload?) {
+
+        let finalPayload;
+        if (payload) {
+            finalPayload = payload;
+        } else {
+            finalPayload = this.pushNotificationData;
+        }
+
+        this.http.post(environment.apiBaseUrl + 'users', finalPayload).subscribe((res) => {
+
+            this.users.count = res['data'].count;
+            this.users.items = res['data'].users;
+            console.log(res);
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+
+    constructor(private http: HttpClient, ) { }
 
   ngOnInit() {
+
+
   }
 
 }
